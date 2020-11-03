c14d7dd4ff62ec50c0e7617caa959ea1470fc4ca
alvinlal <alvinzzz2001@gmail.com>
2020-11-03 14:00:18 +0530

    added selection sort

---
+++ sorting/selectionSort/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (lowest !== i) {
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([8, 17, 4, 2, 10, 7, 3, 4, 6, 5]));
