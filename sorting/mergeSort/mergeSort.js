3b049d86989e9b5f7b18c5065820738fd730d2e8
alvinlal <alvinzzz2001@gmail.com>
2020-11-03 18:53:31 +0530

    added merge sort

---
+++ sorting/mergeSort/mergeSort.js
@@ -0,0 +1,36 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  var mid = Math.floor(arr.length / 2);
+  var left = mergeSort(arr.slice(0, mid));
+  var right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([10, 24, 76, 73, 12, 25, 28, 99]));
+
+function merge(arr1, arr2) {
+  var result = [];
+  var i = 0;
+  var j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
