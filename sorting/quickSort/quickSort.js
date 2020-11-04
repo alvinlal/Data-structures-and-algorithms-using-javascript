865abe7257957cefb0f598585199d321b63cdd60
alvinlal <alvinzzz2001@gmail.com>
2020-11-04 10:20:22 +0530

    added quick sort

---
+++ sorting/quickSort/quickSort.js
@@ -0,0 +1,32 @@
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  var pivot = arr[start];
+  var swapIndex = start;
+
+  for (let i = start; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+
+  swap(arr, start, swapIndex);
+
+  return swapIndex;
+}
+
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
